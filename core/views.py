a587199ba5bfb9ffff9cb3dd867726f5a2a3b4d2
plainte <molnard0412@gmail.com>
2020-11-22 04:26:08 +0100

    Initial setup

---
+++ core/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import Item
+
+# Create your views here.
+def products(request):
+  context = {
+    'items': Item.objects.all()
+  }
+  return render(request, "products.html", context)
+
+def checkout(request):
+  return render(request, "checkout.html")
+
+def home(request):
+  return render(request, "home.html")
